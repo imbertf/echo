e79aeea64cf6e18419fe1fb7be96c093a5ac6b09
flo <f.imbert4@gmail.com>
2024-08-27 08:13:41 +0200

    @ to ../

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import { Inter as FontSans } from "next/font/google"
 import Layout from "../components/shared/Layout";
 
 
-import { cn } from "@/lib/utils"
+import { cn } from "../lib/utils"
 
 const fontSans = FontSans({
   subsets: ["latin"],
